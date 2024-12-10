b3f552e1fa6492d33ef41622a3b7cc8b319251d6
grupo01 <notengocorreo@notengo.com>
2024-12-10 11:51:35 +0100

    Terminadas las operaciones de delete EMpezamos con CSS

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,17 +1,35 @@
 $(function() {
     console.log('Paso por aquí');
+    
     $("#listar").on("click", function() {
         $.get("https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes", function(data) {
             $("#resListar").text("Ok");
+            $("#resListar").attr("data-midato", data);
             console.log(data);
-        })
-        
+        });
     });
+
     $("#leer").on("click", function() {
         $.get("https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/1", function(data) {
             $("#resLeer").text("Ok"); 
             console.log(data);
-        })
-        
-    })
+        });
+    });
+
+    $('#crear').on('click', function() {
+        $.ajax({
+            url: "https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/1",
+            method: "POST",
+            success: function(data) {
+                console.log(data);
+            }
+        });
+    });
+    
+    $("#crear").on("click", function() {
+        $.get("https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/1", function(data) {
+            $("#resLeer").text("Ok"); 
+            console.log(data);
+        });
+    });
 });
